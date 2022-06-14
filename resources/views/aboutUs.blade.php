c202c0379b0631a5c113a3c5906fb08fa9c2866f
rrezik <mariosntouxis1996@gmail.com>
2022-06-14 15:42:58 +0200

    add aboutUS PAGE

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,52 @@
+@extends("templates/layoutTemplate")
+section("content")
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+        body { background-color: lightyellow; }
+       
+    </style>
+
+<body>
+
+    <h1>We are a team of expert designers based in Luxembourg</h1>
+
+    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde nostrum tenetur veniam quidem velit, voluptate corrupti doloremque dolores officiis ab. Cum mollitia, in error architecto vero tempora rem animi odit.</p>
+    
+    <div class="Elisângela">Elisângela <img src="" alt="" style="" ></div>
+    <div class="Lisa">Lisa <img src="" alt="" style="" ></div>
+    <div class="Betty">Betty <img src="" alt="" style="" ></div>
+    <div class="Lucas">Lucas <img src="" alt="" style="" ></div>
+    <div class="Mario">Mario <img src="" alt="" style="" ></div>
+
+   <h2>Mission </h2>
+    <p> MatchHome is the site for real estate listings in Luxembourg and its border areas. Since its creation in 2008, our mission has been to offer the best platform for publishing and searching property listings.
+    We believe that real estate research should not be frustrating. If done with the right tools, it can be a pleasant and rewarding experience. We work for this, join us!</p>
+
+    <h2>Values</h2>
+    Values
+some icon
+We bring innovation to the real estate market, with dedication and enthusiasm. Our mission is to offer the best real estate search service: easily usable by everyone, both on desktops and smartphone, supporting real estate agents in the brokerage activity.
+
+some icon
+We are driven by innovation: we are convinced that what has been done so far in the real estate sector is only an anticipation of big changes in the coming years. New software, more and more hardware powerful, mobile devices and artificial intelligence glimpse high potential, unthinkable even just a few years ago.
+
+some icon
+We aim to thetechnological excellence: we invest a large part of our economic and human resources to ensure that technology makes simple complex activities and that our users can take full advantage of our services. Often complex and more powerful things are those that are not seen.
+
+some icon
+We believe in the service and respect for the users who are looking for properties and professionals who use our portals to publish their offers. If we have achieved such relevant results in such a short time it's been thanks to the support, suggestions and criticisms of those who use the sites of our network.
+    
+
+    
+
+</body>
+</
